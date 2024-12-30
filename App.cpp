3ccd90935e1dac3a7878429572611ece6f8206ac
Mert Can Yılmaz <dokkensavatage@gmail.com>
2024-12-30 21:51:58 +0300

    Add project files.

---
+++ App.cpp
@@ -0,0 +1,31 @@
+#include "App.h"
+#include <sstream>
+#include <iomanip>
+
+App::App()
+	:
+	wnd(800, 600, "Direct3D Example Window")
+{
+}
+
+int App::Go()
+{
+	while (true)
+	{
+		// process all messages pending, but to not block for new messages
+		if (const auto ecode = Window::ProcessMessages())
+		{
+			// if return optional has value, means we're quitting so return exit code
+			return *ecode;
+		}
+		DoFrame();
+	}
+}
+
+void App::DoFrame()
+{
+	const float t = timer.Peek();
+	std::ostringstream oss;
+	oss << "Time elapsed: " << std::setprecision(1) << std::fixed << t << "s";
+	wnd.SetTitle(oss.str());
+}
